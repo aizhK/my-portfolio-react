bc680f06c2d2a3e1e42bc06182a3a49a7ae22fd1
aizhark <aizhark@metropolia.fi>
2023-01-28 14:19:54 +0200

    changes made to data.jsx

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -4,8 +4,10 @@ const Data = () => {
     return (
         <div className="home__data">
             <h1 className="home__title">Aizhar Khassen</h1>
-            <h3 className="home__subtitle">IDK WHAT I AM </h3>
-            <p className="home__description">crying crying crying crying</p>
+            <h3 className="home__subtitle">Student and Aspiring Front-End Developer</h3>
+            <p className="home__description">I am Aizhar, a student pursuing my Bachelors in Information Technology, who's interested in the world of tech. 
+            With skills and knowledge in multiple programming languages, Networking and IT, alongside a creative mindset, 
+            I love to create various frontend products with great user experience.</p>
             <a href="#contact" className="button button--flex">Contact me</a>
         </div>
     );
